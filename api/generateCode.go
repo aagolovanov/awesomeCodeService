f569f46ab24b499dc7eb1e9cbaf384398464874f
Nov1X <angol_02@mail.ru>
2023-04-20 15:32:21 +0300

    json decoders

--- api/generateCode.go
+++ api/generateCode.go
@@ -1,7 +1,21 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/aagolovanov/awesomeCodeService/domain"
+	"net/http"
+)
 
 func generateCode(w http.ResponseWriter, r *http.Request) {
+
+	request := domain.RequestGenerate{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		// TODO write error
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
 }
--- api/verifyCode.go
+++ api/verifyCode.go
@@ -1,8 +1,20 @@
 package api
 
-import "net/http"
+import (
+	"encoding/json"
+	"github.com/aagolovanov/awesomeCodeService/domain"
+	"net/http"
+)
 
 func verifyCode(w http.ResponseWriter, r *http.Request) {
+	request := domain.RequestWithCode{}
+	err := json.NewDecoder(r.Body).Decode(&request)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		// TODO write error
+		return
+	}
+
 	w.WriteHeader(http.StatusOK)
-	return
 }
